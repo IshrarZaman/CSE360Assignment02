e8e5e160c56ce7cb7c3dc4a426fde5c0ff27e16f
Ishrar Zaman <zamanishrar@gmail.com>
2020-10-05 20:52:38 -0500

    Initial check in of Adding Machine.

---
+++ Assignment2/src/cse360assignment02/AddingMachine.java
@@ -0,0 +1,31 @@
+package cse360assignment02;
+
+public class AddingMachine {
+  private int total;
+  
+  public AddingMachine () {
+    total = 0;  // not needed - included for clarity
+  }
+  
+  //returns the current total
+  public int getTotal () {
+    return 0;
+  }
+  
+  //adds integer, value, to the total variable
+  public void add (int value) {
+  }
+  
+  //subtracts integer, value, from the total variable
+  public void subtract (int value) {
+  }
+
+  //prints out all calculations to the total variable
+  public String toString () {
+    return "";
+  }
+
+  //clears memory
+  public void clear() {
+  }
+}
